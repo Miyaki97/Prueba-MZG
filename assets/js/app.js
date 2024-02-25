c25599235ec8f7bdfbca1c39e4e0dc765fee49c9
Hajime <millarayzunigag@gmail.com>
2024-02-25 15:32:18 -0300

    séptimo commit inserción tooltip, jquery ready, toolptiptriggerlist en JS

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+let tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+let tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
